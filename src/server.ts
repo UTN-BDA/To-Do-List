6af25875d2930f261c29e85c6eb87e0bc50ea510
Eliezer <eliriper123@gmail.com>
2025-07-23 12:44:38 -0300

    Sanitizado Mongo

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,16 @@ import Express, { Router } from 'express';
 import connectMongo from './config/dbMongo';
 import router from './routers/taskRoutes'; 
 import cors from 'cors';
-
+import mongoSanitizer from 'express-mongo-sanitize';
 connectMongo()
 
-const cors=require('cors')
 
 // Create an Express server
 const server = Express();
 
 // Middleware
 server.use(Express.json());
+server.use(mongoSanitizer());
 server.use(cors({ origin: 'http://localhost:3000' }))
 
 server.use('/api/lista',router)
